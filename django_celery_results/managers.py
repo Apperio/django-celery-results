e0731da89be25187eec989603dce8fab622065bf
nadianasato <nadia.nasato@apperio.com>
2021-03-24 13:53:22 +0000

    Delete results immediately, not in two steps.
    See https://github.com/celery/django-celery-results/pull/118

--- django_celery_results/managers.py
+++ django_celery_results/managers.py
@@ -158,9 +158,4 @@ class TaskResultManager(models.Manager):
         """Delete all expired results."""
         meta = self.model._meta
         with transaction.atomic():
-            self.get_all_expired(expires).update(hidden=True)
-            cursor = self.connection_for_write().cursor()
-            cursor.execute(
-                'DELETE FROM {0.db_table} WHERE hidden=%s'.format(meta),
-                (True, ),
-            )
+            self.get_all_expired(expires).delete()
